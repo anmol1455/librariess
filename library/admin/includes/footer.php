90efa35786c0a41670171892a581b6180fdb02b9
anmol1455 <anmol.write@gmail.com>
2021-08-19 13:54:37 +0530

    Initial commit

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Library Management System | Designed by : Manisha Gupta</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
